4c347cf432a6056a2e1a879ebdc4064ae9c2e331
Ally-hy-K <83394488+Ally-hy-K@users.noreply.github.com>
2021-05-07 21:18:59 +0800

    Update bidding_system.sol

--- bidding_system.sol
+++ bidding_system.sol
@@ -26,6 +26,36 @@ contract AdoptionBidding {
 
     event AuctionEnded(address winner, uint highestBid);
     
+   
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+///Open the channel for transation
+contract PaymentChannel {
+  address payable public applicants
+  address payable public admin
+  uint256 public expiration
+  
+  constructor(address payable_university, uint256 duration)
+    public
+    payable
+    
+  {
+    applicants = msg.sender
+    admin = _admin
+    expiration = now+duration
+  }
+
+
+
+
+/// Closing the channel after the transaction
+function close(unit token, bytes 32)public(
+  require(msg.sender == university);        //validate the action owner is the admin
+  require(winner.length > 0);               //validate the winner exists
+  require(animal.length > 0);               // valudate the animal exists
+  selfdestruct(applicants);                 // delete other applicants
+ }
+    
     
     
     
